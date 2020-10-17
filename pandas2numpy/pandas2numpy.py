c5148f6a425824456b3c7f118fda0ecb8508aef9
Nestor Demeure <nestordemeure@gmail.com>
2020-10-17 19:19:58 +0200

    categorial encoding and decoding is now working

--- pandas2numpy/pandas2numpy.py
+++ pandas2numpy/pandas2numpy.py
@@ -19,8 +19,10 @@ class Pandas2numpy():
         self.normalized_columns_means = transformed_df[self.normalized_columns].mean(skipna=True)
         self.normalized_columns_std = transformed_df[self.normalized_columns].std(skipna=True)
         # stores info on categories
-        self.nb_label_per_category = 0 # TODO
-    
+        self.category_dtypes = dataframe[self.categorial_columns].astype('category').dtypes
+        # TODO add 1 to NA categories
+        self.nb_label_per_category = self.category_dtypes.apply(lambda cat: len(cat.categories)).to_numpy()
+
     #--------------------------------------------------------------------------
     # ENCODING
 
@@ -33,21 +35,21 @@ class Pandas2numpy():
         # normalizes some columns
         df[self.normalized_columns] = (df[self.normalized_columns] - self.normalized_columns_means) / self.normalized_columns_std
         return df.to_numpy()
-    
+
     def categorial_to_numpy(self, df):
-        # TODO use predefined categories
-        # NA become -1 unless converted to string before cat?
-        # could just add 1 to code in this case
-        tensor = df[self.categorial_columns].astype('category') \
-                                            .apply(lambda x: x.cat.codes) \
-                                            .to_numpy()
-        return tensor
+        # encodes data as categories using predefined categories
+        df = df[self.categorial_columns].astype(self.category_dtypes) \
+                                        .apply(lambda x: x.cat.codes) \
+        # NA have code -1 by default, insures all codes are positive
+        df[self.NA_cat_columns] += 1
+        # TODO add NA cont columns
+        return df.to_numpy()
 
     def to_numpy(self, df):
         tensor_cont = self.continuous_to_numpy(df)
         tensor_cat = self.categorial_to_numpy(df)
         return (tensor_cont, tensor_cat)
-    
+
     #--------------------------------------------------------------------------
     # DECODING
 
@@ -61,20 +63,27 @@ class Pandas2numpy():
         df[self.logscale_columns] = df[self.logscale_columns].apply(np.exp)
         # nothing to do to reinject NAs at this stage
         return df
-    
-    def categorial_from_numpy(self, tensor_cat, copy=True):
-        """"use copy to avoid side effects"""
-        df = pd.DataFrame(data=tensor_cat, columns=self.categorial_columns, copy=copy)
-        # TODO removes NA columns
-        # TODO converts from code to categories
-        # pd.Categorical.from_codes(codes, categories=["train", "test"])
+
+    def categorial_from_numpy(self, tensor_cat):
+        columns = []
+        # decodes one columns at a time
+        for (col_index, col_name) in enumerate(self.categorial_columns):
+            codes = tensor_cat[:,col_index]
+            # gets NA back to code -1
+            if col_name in self.NA_cat_columns: codes -= 1
+            # translates codes in to their categories
+            categories = self.category_dtypes[col_index].categories
+            column = pd.Categorical.from_codes(codes, categories=categories)
+            columns.append(column)
+        df = pd.DataFrame(data=columns, columns=self.categorial_columns)
+        # TODO deal with NA columns
         return df
 
     def from_numpy(self, tensor_cont, tensor_cat, copy=True):
-        """"use copy to avoid side effects"""
+        """use copy to avoid side effects"""
         df_cont = self.continuous_from_numpy(tensor_cont, copy=copy)
-        df_cat = self.categorial_from_numpy(tensor_cat, copy=copy)
+        df_cat = self.categorial_from_numpy(tensor_cat)
         # merge dataframes
         df = pd.concat((df_cont, df_cat), axis=1)
-        # TODO take care of cont NA columns
+        # TODO remove NA columns
         return df
