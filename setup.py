667443b06b3dae2fb723594ab53cd0f369416b4d
Nestor Demeure <nestordemeure@gmail.com>
2021-03-11 20:05:24 +0100

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 # https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
   name = 'pandas2numpy',
-  packages = ['pandas2numpy'],
   version = '1.0',
   license='apache-2.0',
   description = 'Dataframe to tensor converter for deep learning.',
   author = 'NestorDemeure',
-#  author_email = 'your.email@domain.com',
-  url = 'https://github.com/nestordemeure/tabularGP',
+  #  author_email = 'your.email@domain.com',
+  url = 'https://github.com/nestordemeure/pandas2numpy',
   keywords = ['pandas', 'numpy', 'tabular-data', 'deep-learning'],
   install_requires=[
           'numpy',
@@ -23,4 +22,5 @@ setup(
     'License :: OSI Approved :: Apache Software License',
     'Programming Language :: Python :: 3 :: Only',
   ],
+  packages=find_packages(),
 )
